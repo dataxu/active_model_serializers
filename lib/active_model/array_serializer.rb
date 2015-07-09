e2a595af83ed44428cc1d43eacc9ea724a78f49a
Anthony White <awhite@dataxu.com>
2015-07-09 09:52:06 -0700

    ArraySerializer properly passes options through
    Fixes an issue where individual serializers were not receiving
    options such as 'only' from an ArraySerializer

--- lib/active_model/array_serializer.rb
+++ lib/active_model/array_serializer.rb
@@ -20,8 +20,9 @@ module ActiveModel
       @meta            = options[@meta_key]
       @each_serializer = options[:each_serializer]
       @resource_name   = options[:resource_name]
+      @options         = options
     end
-    attr_accessor :object, :scope, :root, :meta_key, :meta
+    attr_accessor :object, :scope, :root, :meta_key, :meta, :options
 
     def json_key
       if root.nil?
@@ -33,7 +34,10 @@ module ActiveModel
 
     def serializer_for(item)
       serializer_class = @each_serializer || Serializer.serializer_for(item) || DefaultSerializer
-      serializer_class.new(item, scope: scope)
+      serializer_class.new(
+        item,
+        options.slice(:scope, :only, :except, :root, :meta_key, :meta, :wrap_in_array)
+      )
     end
 
     def serializable_object
