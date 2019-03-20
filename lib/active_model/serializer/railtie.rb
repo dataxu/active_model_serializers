e537837ffde7ccb732361e4055c0c88a9c98452c
Jeff Luckett <jluckett@dataxu.com>
2019-03-20 15:57:19 -0400

    Don't need 'em

--- lib/active_model/serializer/railtie.rb
+++ lib/active_model/serializer/railtie.rb
@@ -1,10 +1,10 @@
 module ActiveModel
   class Railtie < Rails::Railtie
     initializer 'generators' do |app|
-      app.load_generators
-      require 'active_model/serializer/generators/serializer/serializer_generator'
-      require 'active_model/serializer/generators/serializer/scaffold_controller_generator'
-      require 'active_model/serializer/generators/resource_override'
+      # app.load_generators
+      # require 'active_model/serializer/generators/serializer/serializer_generator'
+      # require 'active_model/serializer/generators/serializer/scaffold_controller_generator'
+      # require 'active_model/serializer/generators/resource_override'
     end
   end
 end
